716b44d68e1e32b4156a54aeeb5503006e0472fe
Ryan Waszak <rwaszak@tesla.com>
2021-04-22 11:09:18 -0700

    dependency injection and add controller

--- GraphQlProject/Startup.cs
+++ GraphQlProject/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraphQlProject.Interfaces;
+using GraphQlProject.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,7 @@ namespace GraphQlProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<IProduct, ProductService>()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
